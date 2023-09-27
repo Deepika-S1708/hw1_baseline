35e5740ebc4ec67dce4cbf845bee159a27aa5ddb
Deepika-S1708 <deepikavelu1703@gmail.com>
2023-09-27 21:36:42 +0530

    Added InputValidation files

---
+++ expense_tracker/src/InputValidation.java
@@ -0,0 +1,27 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.awt.Component;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class InputValidation {
+    public InputValidation() {
+    }
+
+    public void intake_verify(double amount, String category) {
+        if (amount < 0.0 | amount > 1000.0) {
+            JOptionPane.showMessageDialog((Component)null, "Error ! The amount should be greater than 0 and less than 1000");
+            throw new IllegalArgumentException("Error! The amount should be greater than 0 and less than 1000");
+        } else {
+            List<String> valid_list_of_items = Arrays.asList("food", "travel", "bills", "entertainment", "other");
+            if (!valid_list_of_items.contains(category)) {
+                JOptionPane.showMessageDialog((Component)null, "Error! The category must be from \"food\", \"travel\", \"bills\", \"entertainment\",\"other\"");
+                throw new IllegalArgumentException("Error! The category must be from \"food\",\"travel\", \"bills\", \"entertainment\",\"other\"");
+            }
+        }
+    }
+}
